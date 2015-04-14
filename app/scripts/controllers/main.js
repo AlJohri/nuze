5602a0fa606ba23e4bb67465bcfc6001f24dcbc2
Al Johri <al.johri@gmail.com>
2015-04-14 06:01:15 -0500

    remove angularfire chat scaffold

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,16 @@
  * Controller of the nuzeApp
  */
 angular.module('nuzeApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, Ref, $firebaseArray, $timeout) {
+
+    $scope.messages = $firebaseArray(Ref.child('messages').limitToLast(10));
+    $scope.messages.$loaded().catch(alert);
+
+    function alert(msg) {
+      $scope.err = msg;
+      $timeout(function() {
+        $scope.err = null;
+      }, 5000);
+    }
+
   });
