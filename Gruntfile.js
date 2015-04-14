424194147ca46ae2feaf77196c616ac79645344b
Al Johri <al.johri@gmail.com>
2015-04-14 05:54:46 -0500

    add grunt-build-control

--- Gruntfile.js
+++ Gruntfile.js
@@ -441,7 +441,23 @@ module.exports = function (grunt) {
         configFile: 'test/karma.conf.js',
         singleRun: true
       }
+    },
+
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:AlJohri/nuze.git',
+          branch: 'gh-pages'
+        }
+      },
     }
+
   });
 
 
