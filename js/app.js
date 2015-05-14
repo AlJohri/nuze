b469e0181e7123041de81089150e95302064cda0
Al Johri <al.johri@gmail.com>
2015-05-13 23:00:45 -0500

    make yik yak actually work

--- js/app.js
+++ js/app.js
@@ -1,24 +1,38 @@
 $(function(){
 
-    // YikYak Views
-
-    var YakListView = Backbone.View.extend({
-        limit: 10,
-        el: "#yak_list",
+    var FeedView = Backbone.View.extend({
+        el: "#feed",
+        initialize: function(options) {
+            this.collections = options.collections;
+            // this.listenTo(this.collections.YakList, 'add', this.renderOne);
+        },
+        renderOneYak: function(yak) {
+            var self = this;
+            var $iv = new YakItemView({model:yak}).render().$el;
+            self.$el.prepend($iv);
+        },
         render: function() {
             var self = this;
-            this.collection.each(function(story) {
-                var $iv = new YakItemView({model:story}).render().$el;
-                self.$el.prepend($iv);
+            _.each(_.values(this.collections), function(collection) {
+                collection.each(function(story) {
+                    // debugger;
+                    if(collection.name == "RSSItem") {
+                        var $iv = new RSSItemView({model:story}).render().$el;
+                    }
+                    else {
+                        var $iv = new YakItemView({model:story}).render().$el;
+                    }
+                    self.$el.prepend($iv);
+                });
             });
         }
     })
 
     var YakItemView = Backbone.View.extend({
         template: $('#yak_item').html(),
-        initialize: function() {
-            this.listenTo(this.model, "change", this.render);
-        },
+        // initialize: function() {
+        //     this.listenTo(this.model, "change", this.render);
+        // },
         render: function() {
             var variables = _.extend({cid:this.model.cid}, this.model.toJSON());
             var template = _.template(this.template, variables);
@@ -27,22 +41,12 @@ $(function(){
         }
     })
 
-    // RSS Views
-
-    var RSSListView = Backbone.View.extend({
-        el: "#rss_list",
-        render: function() {
-            var self = this;
-            this.collection.each(function(story) {
-                var $iv = new RSSItemView({model:story}).render().$el;
-                self.$el.prepend($iv);
-            });
-        }
-    });
-
     var RSSItemView = Backbone.View.extend({
         events: { "click a.story": "clickedStory" },
         template: $('#story_item').html(),
+        // initialize: function() {
+        //     this.listenTo(this.model, "change", this.render);
+        // },
         render: function() {
             var variables = _.extend({cid:this.model.cid}, this.model.toJSON());
             var template = _.template(this.template, variables);
@@ -54,38 +58,43 @@ $(function(){
     // RSS Models / Collections
 
     var RSSItem = Backbone.Model.extend({ idAttribute: "url" });
-    var RSSList = Backbone.Collection.extend({ model: RSSItem });
+    var RSSList = Backbone.Collection.extend({
+        initialize: function() {
+            this.name = "RSSItem";
+        },
+        model: RSSItem,
+    });
 
     // Yik Yak Models / Collections
 
     var YakItem = Backbone.Model.extend({ });
     var YakList = Backbone.Firebase.Collection.extend({
-      url: new Firebase('https://aljohri-nuyak.firebaseio.com/yaks').limitToLast(10),
-      model: YakItem,
-      autoSync: true
+        initialize: function() {
+            this.name = "YakItem";
+        },
+        url: new Firebase('https://aljohri-nuyak.firebaseio.com/yaks').limitToLast(10),
+        model: YakItem,
+        autoSync: true
     });
 
     // Populate Models
 
-    var yaks = new YakList();
-    yaks.on('sync', function(collection) {
+    var feedview = new FeedView({ collections: {
+        RSSList : new RSSList(),
+        YakList : new YakList()
+    }});
+
+    feedview.collections.YakList.on('sync', function(collection) {
       console.log('yaks are loaded', collection);
-      list2.render();
     });
 
-    var list2 = new YakListView({collection:yaks});
+    feedview.collections.YakList.on('add', function(yak) {
+      console.log('new yak', yak);
+      feedview.renderOneYak(yak);
+    });
 
-    // yaks.on('all', function(event) {
-    //   // if autoSync is true this will log remove and sync
-    //   // if autoSync is false this will only log remove
-    //   console.log(event);
-    // });
-
-    // Application
-    var coll = new RSSList();
     var feed = "http://dailynorthwestern.com/feed/"
     console.log("fetching feed...");
-    var list = new RSSListView({collection:coll});
 
     $.ajax({
         url: "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=6",
@@ -94,15 +103,12 @@ $(function(){
             q: feed
         },
         success: function(data) {
-
-            _(data.responseData.feed.entries).each(function(entry){
+            _(data.responseData.feed.entries).each(function(entry) {
                 console.log(entry.title);
                 var m = new RSSItem({title:entry.title, desc:entry.content, url: entry.link});
-                coll.add(m);
+                feedview.collections.RSSList.add(m);
             });
-
-            list.render();
-
+            feedview.render();
         }
 
     });
@@ -110,7 +116,5 @@ $(function(){
     var isValidFeed = function(url) {
         return true;
     };
-
-    // console.log(yaks.first());
 
 });
