b55dabb453e7f5d7dc345f338f1a7a4d48b1335a
Al Johri <al.johri@gmail.com>
2015-06-03 12:28:17 -0500

    remove old code for yik yak with firebase

--- js/app.js
+++ js/app.js
@@ -70,12 +70,6 @@ var TweetItem = Backbone.Model.extend({
 });
 
 var YakItem = Backbone.Model.extend({
-    // constructor: function() {
-    //     arguments[0].date = new Date(arguments[0].time);
-    //     arguments[0].text = arguments[0].message;
-    //     arguments[0].score = Math.floor(arguments[0].likes/4);
-    //     Backbone.Model.apply(this, arguments);
-    // },
     idAttribute: "id",
     defaults: { source: "Yik Yak", logo: "img/yikyaklogo.png", message: "" }
 });
